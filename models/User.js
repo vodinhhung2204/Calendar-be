a96141cbc61c81a91d1a012df065a78c461c3a20
Le Hoang Huy <lehoanghuyit1009@gmail.com>
2019-07-03 14:38:57 +0700

    [Feature/api-register] API registration.

--- models/User.js
+++ models/User.js
@@ -1,16 +1,32 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
+const bcrypt = require('bcrypt');
 
-const Role = require('./Role');
-
-const { Schema } = mongose;
-
-const User = new Schema({
-  username: { type: String },
-  password: { type: String },
-  fullname: { type: String },
-  email: { type: String },
-  nameRole: [Role.nameRole]
+const emailValidator = [
+  val => validator.isEmail(val),
+  'Email is incorrect format',
+];
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: 'Username is required',
+  },
+  password: {
+    type: String,
+    required: 'Username is required',
+  },
+  fullname: {
+    type: String,
+    required: 'Fullname is required',
+  },
+  email: {
+    type: String,
+    required: 'Email is required',
+    validate: emailValidator,
+  },
+  role: { type: String },
 });
+userSchema.methods.comparePassword = (password, encode) => bcrypt.compareSync(password, encode);
+userSchema.methods.getHash = password => bcrypt.hashSync(password, 10);
 
-
-module.exports = mongoose.model('User', User);
+module.exports = mongoose.model('User', userSchema);
