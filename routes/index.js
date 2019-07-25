3abacb4c9a2fd8f10b0da24f4b869ef8a263a019
Le Hoang Huy <lehoanghuyit1009@gmail.com>
2019-07-25 15:14:38 +0700

    api home page config

--- routes/index.js
+++ routes/index.js
@@ -6,14 +6,14 @@ const habitController = require("../controllers/HabitController");
 
 router.route("/")
   .get((req, res) => {
-    const idUserLogin = jwt.decode(req.headers.authorization.split(" ")[1]).payload;
-    return habitController.getItemsByUserID(idUserLogin)
+    const userIDLogin = jwt.decode(req.headers.authorization.split(" ")[1]).userID;
+    return habitController.getItemsByUserID(userIDLogin)
       .then(result => res.status(200).json(result))
       .catch(err => res.status(400).json({ message: err.message }));
   });
 router.route("/habit")
   .get((req, res) => {
-    const userIDLogin = jwt.decode(req.headers.authorization.split(" ")[1]).payload;
+    const userIDLogin = jwt.decode(req.headers.authorization.split(" ")[1]).userID;
     const habitID = req.query.id;
     return habitController.getListHabitByHabitIDAndUserID(habitID, userIDLogin)
       .then(result => res.status(200).json(result))
